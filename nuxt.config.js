d349fd8c60d53666ccc97d1cf33e10c77456b9f2
Piyush Goyani <piyush.goyani@ezeetechnosys.com>
2021-06-23 15:54:52 +0530

    base url change

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,6 +3,9 @@ import colors from 'vuetify/es5/util/colors'
 export default {
   ssr: false,
   target: 'static',
+  router: {
+    base: '/video-player/',
+  },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: '%s - video-player',
