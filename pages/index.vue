f39eb611843cbe05f2b8bc28bf72300a2626460d
Piyush Goyani <piyush.goyani@ezeetechnosys.com>
2021-06-23 13:52:21 +0530

    add player input and render content, check hostname by parse, check URL connectivity, check mimeType, remove unused files

--- pages/index.vue
+++ pages/index.vue
@@ -2,72 +2,57 @@
   <v-row justify="center" align="center">
     <v-col cols="12" sm="8" md="6">
       <div class="text-center">
-        <logo />
-        <vuetify-logo />
+        <p>test</p>
       </div>
       <v-card>
         <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
+          TITLE
         </v-card-title>
         <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              documentation
-            </a>.
-          </p>
-          <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord
-            </a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board
-            </a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
+          <v-text-field
+            v-model="url"
+            append-outer-icon="mdi-arrow-right"
+            @click:append-outer="validateURL"
+          />
+          <div v-if="isContentValidated">
+            <template v-if="hostType === 'youtube'">
+              <video
+                v-if="response != 'Invalid data' && response != null"
+                width="700"
+                height="500"
+                autoplay
+                :src="
+                  `https://www.youtube.com//${contentId}?key=AIzaSyC_28L2bV2wGcZZqk_0NbReNJBNV4V5BNI`
+                "
+              />
+            </template>
+            <template v-else-if="hostType === 'gdrive'">
+              <video
+                v-if="response != 'Invalid data' && response != null"
+                width="700"
+                height="500"
+                autoplay
+                :src="
+                  `https://www.googleapis.com/drive/v3/files/${contentId}?key=AIzaSyC_28L2bV2wGcZZqk_0NbReNJBNV4V5BNI`
+                "
+              />
+            </template>
+            <template v-else-if="hostType === 'custom'">
+              <video
+                v-if="response != 'Invalid data' && response != null"
+                width="700"
+                height="500"
+                autoplay
+                :src="
+                  `https://www.googleapis.com/drive/v3/files/${contentId}?key=AIzaSyC_28L2bV2wGcZZqk_0NbReNJBNV4V5BNI`
+                "
+              />
+            </template>
           </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt Documentation
-          </a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt GitHub
-          </a>
         </v-card-text>
         <v-card-actions>
           <v-spacer />
-          <v-btn
-            color="primary"
-            nuxt
-            to="/inspire"
-          >
+          <v-btn color="primary">
             Continue
           </v-btn>
         </v-card-actions>
@@ -77,13 +62,51 @@
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
-
 export default {
-  components: {
-    Logo,
-    VuetifyLogo,
+  data () {
+    return {
+      url: '',
+      hostType: null,
+      mimeType: null,
+      contentId: null,
+      isContentValidated: false,
+    }
+  },
+  methods: {
+    async validateURL () {
+      this.hostType = await this.checkHostName(new URL(this.url).hostname)
+      this.mimeType = await this.checkURLConnectivity(this.url)
+      this.isContentValidated = [
+        'video/mp4',
+        'video/ogg',
+        'video/webm',
+      ].includes(this.mimeType)
+    },
+    checkHostName (hostname) {
+      if (!hostname) {
+        return false
+      }
+      if (hostname.includes('youtube')) {
+        return 'youtube'
+      } else if (hostname.includes('drive')) {
+        return 'gdrive'
+      } else {
+        return 'custom'
+      }
+    },
+    checkURLFormat () {},
+    checkURLConnectivity (url) {
+      return new Promise(function (resolve) {
+        const xhttp = new XMLHttpRequest()
+        xhttp.open('HEAD', url)
+        xhttp.onreadystatechange = function () {
+          if (this.readyState === this.DONE) {
+            resolve(this.getResponseHeader('Content-Type'))
+          }
+        }
+        xhttp.send()
+      })
+    },
   },
 }
 </script>
